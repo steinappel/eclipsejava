da2ae896a155447a08df40d0f1666c624584dbf2
Stein de Bever <steindebever@MacBook-Pro-van-Stein.local>
2020-07-22 19:56:49 +0200

    opdracht 8

---
+++ demo/java/opdracht8.java
@@ -0,0 +1,30 @@
+package demo.java;
+
+public class opdracht8 {
+
+	public static void main(String[] args) {
+
+	       int number = 5;
+
+	       int fact = number;
+	       
+	       for (int i = number ; i > 1 ; i-- ) {
+	    	     
+	    	   fact = fact * (i-1) ;
+	    	   
+	       }
+	       
+	       System.out.println(fact);
+
+
+	       // compute factorial of number and assign result to fact
+
+	       // assume number >= 1
+
+
+	       // print fact
+
+
+	   }
+
+}
