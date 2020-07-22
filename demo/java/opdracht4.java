b66f14a1929dd284491c38580a817f36cdfa5a94
Stein de Bever <steindebever@MacBook-Pro-van-Stein.local>
2020-07-22 16:29:33 +0200

    opdracht 4

---
+++ demo/java/opdracht4.java
@@ -0,0 +1,31 @@
+package demo.java;
+
+public class opdracht4 {
+
+	public static void main(String[] args) {
+		
+		String s = "Deze regel heeft best wel veel eees.";
+		int k = 0;
+		
+		for (int i = 0; i < s.length(); i++){
+			if(s.substring(i, i+1).equals("e")) {
+				 k++;
+				
+			}
+			     
+		    //Process char
+		
+		}
+		
+		System.out.println("De letter e komt: " + k+ " keer voor.");
+
+
+	       // Tel hoe vaak de letter "e" voorkomt in string org
+
+
+	       // Print aantal "e"-s in org
+		// TODO Auto-generated method stub
+
+	}
+
+}
