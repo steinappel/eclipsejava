04d641497f6ab84023e708d78e170a60655194a5
Stein de Bever <steindebever@MacBook-Pro-van-Stein.local>
2020-07-22 16:02:59 +0200

    opdracht 2

---
+++ demo/java/opdracht2.java
@@ -0,0 +1,17 @@
+package demo.java;
+
+public class opdracht2 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int x = 8;
+
+		int y = 2;
+		System.out.println(x+ y);
+		System.out.println(x-y);
+		
+
+	}
+	
+}
